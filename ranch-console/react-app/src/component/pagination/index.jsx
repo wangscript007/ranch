8158fdd3f8868d4344108d0245b7ffb258b03bb7
lpw <heisedebaise@hotmail.com>
2017-07-21 10:14:17 +0800

    开发组件、页面。

--- ranch-console/react-app/src/component/pagination/index.jsx
+++ ranch-console/react-app/src/component/pagination/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./index.css";
+import message from "./message.json";
 
 class Pagination extends React.Component {
     render() {
         return (
-            <div className="pagination">分页组件</div>
+            <div className="pagination">
+                {this.page(2, "<", "pagination.prev")}
+                {this.page(1, 1)}
+                {this.page(2, 2)}
+                <span>3</span>
+                {this.page(4, 4)}
+                {this.page(5, 5)}
+                {this.page(4, ">", "pagination.next")}
+            </div>
+        );
+    }
+
+    page(num, label, title) {
+        return (
+            // eslint-disable-next-line
+            <a href="javascript:void(0);" title={window.message(message, title)}>{label}</a>
         );
     }
 }
--- ranch-console/react-app/src/mock/menu.js
+++ ranch-console/react-app/src/mock/menu.js
@@ -1,6 +1,15 @@
 class Menu {
     mock(mock) {
         mock.put("/console/menu", "", [{
+            "name": "主菜单",
+            "items": [{
+                "name": "子菜单一",
+                "service": "service key"
+            }, {
+                "name": "子菜单二",
+                "service": "ranch.account.query"
+            }]
+        }, {
             "name": "系统设置",
             "items": [{
                 "name": "用户管理",
