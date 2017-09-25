3175f55f04b73a15a4e2cad9fe307dc22f7b223f
lpw <heisedebaise@hotmail.com>
2017-09-25 11:30:18 +0800

    Console访问添加基础权限控制。

---
+++ ranch-console/react-app/src/console/bottom.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./bottom.css";
+
+class Bottom extends React.Component {
+    render() {
+        return (
+            <div className="console-bottom">
+                <div>Power by <a href="https://github.com/heisedebaise/ranch/tree/master/ranch-console" target="_blank" rel="noopener noreferrer">ranch-console</a></div>
+            </div>
+        );
+    }
+}
+
+export default Bottom;
