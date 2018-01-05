12e4de32d9dda3dc1696e65e565ffb846faa575c
lpw <heisedebaise@hotmail.com>
2018-01-05 09:03:04 +0800

    开发控制台模块。

--- ranch-console/src/main/java/org/lpw/ranch/console/PermitValidatorImpl.java
+++ ranch-console/src/main/java/org/lpw/ranch/console/PermitValidatorImpl.java
@@ -1,13 +1,24 @@
 package org.lpw.ranch.console;
 
+import org.lpw.tephra.ctrl.validate.ValidateWrapper;
 import org.lpw.tephra.ctrl.validate.ValidatorSupport;
 import org.springframework.stereotype.Controller;
+
+import javax.inject.Inject;
 
 /**
  * @author lpw
  */
 @Controller(ConsoleService.VALIDATOR_PERMIT)
 public class PermitValidatorImpl extends ValidatorSupport {
+    @Inject
+    private ConsoleService consoleService;
+
+    @Override
+    public boolean validate(ValidateWrapper validate, String parameter) {
+        return consoleService.permit();
+    }
+
     @Override
     protected String getDefaultFailureMessageKey() {
         return ConsoleModel.NAME + ".not-permit";
