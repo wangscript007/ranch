aae7cf314eede3c4e9f3af0ea6d306ec483adbe7
lpw <heisedebaise@hotmail.com>
2018-04-10 16:50:34 +0800

    完善编辑器模块。

--- ranch-editor-helper/src/main/java/org/lpw/ranch/editor/helper/EditorHelper.java
+++ ranch-editor-helper/src/main/java/org/lpw/ranch/editor/helper/EditorHelper.java
@@ -2,9 +2,23 @@ package org.lpw.ranch.editor.helper;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.Map;
+
 /**
  * @author lpw
  */
 public interface EditorHelper {
-    JSONObject save();
+    /**
+     * 保存编辑器信息。
+     *
+     * @param type    类型。
+     * @param name    名称。
+     * @param keyword 关键词。
+     * @param width   宽度。
+     * @param height  高度。
+     * @param image   预览图。
+     * @param map     扩展属性集。
+     * @return 编辑器信息。
+     */
+    JSONObject save(String type, String name, String keyword, int width, int height, String image, Map<String, String> map);
 }
--- ranch-editor-helper/src/main/java/org/lpw/ranch/editor/helper/EditorHelperImpl.java
+++ ranch-editor-helper/src/main/java/org/lpw/ranch/editor/helper/EditorHelperImpl.java
@@ -1,15 +1,42 @@
 package org.lpw.ranch.editor.helper;
 
 import com.alibaba.fastjson.JSONObject;
+import org.lpw.ranch.util.Carousel;
+import org.lpw.tephra.util.Numeric;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author lpw
  */
 @Controller("ranch.editor.helper")
 public class EditorHelperImpl implements EditorHelper {
+    @Inject
+    private Numeric numeric;
+    @Inject
+    private Carousel carousel;
+    @Value("${ranch.editor.key:ranch.editor}")
+    private String key;
+    private String saveKey;
+
     @Override
-    public JSONObject save() {
-        return null;
+    public JSONObject save(String type, String name, String keyword, int width, int height, String image, Map<String, String> map) {
+        if (saveKey == null)
+            saveKey = key + ".save";
+
+        if (map == null)
+            map = new HashMap<>();
+        map.put("type", type);
+        map.put("name", name);
+        map.put("keyword", keyword);
+        map.put("width", numeric.toString(width, "0"));
+        map.put("height", numeric.toString(height, "0"));
+        map.put("image", image);
+
+        return carousel.service(saveKey, null, map, false, JSONObject.class);
     }
 }
