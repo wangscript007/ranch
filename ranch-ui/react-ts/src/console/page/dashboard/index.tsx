b59ac6ec053874a0960e99ef94384f3a6cc1fb9c
lpw <heisedebaise@hotmail.com>
2018-04-12 22:21:26 +0800

    完善UI组件。

--- ranch-ui/react-ts/src/console/page/dashboard/index.tsx
+++ ranch-ui/react-ts/src/console/page/dashboard/index.tsx
@@ -1,40 +1,9 @@
 import * as React from 'react';
-import Radio from '../../../ui/radio';
-import Check from '../../../ui/check';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
-            <div>
-            <Radio name="radio" list={[
-                {
-                    value:"1",
-                    label:"radio 1"
-                },
-                {
-                    value:"2",
-                    label:"radio 2"
-                },
-                {
-                    value:"3",
-                    label:"radio 3"
-                }
-            ]}/>
-            <Check name="check" list={[
-                {
-                    value:"1",
-                    label:"check 1"
-                },
-                {
-                    value:"2",
-                    label:"check 2"
-                },
-                {
-                    value:"3",
-                    label:"check 3"
-                }
-            ]}/>
-            </div>
+            <div>Dashboard</div>
         );
     }
 }
