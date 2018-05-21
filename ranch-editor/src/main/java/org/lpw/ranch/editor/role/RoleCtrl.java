53fe4f6f6754b9b2aa83ca08b53f1a85d67af360
lpw <heisedebaise@hotmail.com>
2018-05-21 14:49:52 +0800

    完善编辑器模块。

--- ranch-editor/src/main/java/org/lpw/ranch/editor/role/RoleCtrl.java
+++ ranch-editor/src/main/java/org/lpw/ranch/editor/role/RoleCtrl.java
@@ -1,9 +1,11 @@
 package org.lpw.ranch.editor.role;
 
+import org.lpw.ranch.editor.EditorService;
 import org.lpw.ranch.user.helper.UserHelper;
 import org.lpw.tephra.ctrl.context.Request;
 import org.lpw.tephra.ctrl.execute.Execute;
 import org.lpw.tephra.ctrl.validate.Validate;
+import org.lpw.tephra.ctrl.validate.Validators;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -20,9 +22,11 @@ public class RoleCtrl {
     private RoleService roleService;
 
     @Execute(name = "delete", validates = {
+            @Validate(validator = Validators.ID, parameter = "editor", failureCode = 41),
             @Validate(validator = UserHelper.VALIDATOR_SIGN_IN),
-            @Validate(validator = RoleService.VALIDATOR_EDITABLE, parameter = "editor", failureCode = 42),
-            @Validate(validator = RoleService.VALIDATOR_DELETABLE, parameters = {"user", "editor"}, failureCode = 43),
+            @Validate(validator = EditorService.VALIDATOR_EXISTS, parameter = "editor", failureCode = 42),
+            @Validate(validator = RoleService.VALIDATOR_EDITABLE, parameter = "editor", failureCode = 43),
+            @Validate(validator = RoleService.VALIDATOR_DELETABLE, parameters = {"user", "editor"}, failureCode = 44),
     })
     public Object delete() {
         roleService.delete(request.get("user"), request.get("editor"));
