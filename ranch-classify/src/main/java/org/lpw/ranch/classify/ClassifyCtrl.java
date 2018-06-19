7f1dc8447346c32a28c51097ec5168b0e1bfea83
heisedebaise <heisedebaise@hotmail.com>
2018-06-19 16:58:10 +0800

    完善分类模块。

--- ranch-classify/src/main/java/org/lpw/ranch/classify/ClassifyCtrl.java
+++ ranch-classify/src/main/java/org/lpw/ranch/classify/ClassifyCtrl.java
@@ -57,36 +57,6 @@ public class ClassifyCtrl extends RecycleCtrlSupport {
         return classifyService.list(request.get("code"), request.get("key"), request.get("name"));
     }
 
-    @Execute(name = "create", validates = {
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "code", failureCode = 2),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "code", failureCode = 3),
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "key", failureCode = 4),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "key", failureCode = 5),
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "name", failureCode = 6),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "name", failureCode = 7),
-            @Validate(validator = Validators.SIGN),
-            @Validate(validator = ClassifyService.VALIDATOR_CODE_KEY_NOT_EXISTS, parameters = {"code", "key"}, failureCode = 10)
-    })
-    public Object create() {
-        return templates.get().success(classifyService.create(request.get("code"), request.get("key"), request.get("value"), request.get("name"), request.getMap()), null);
-    }
-
-    @Execute(name = "modify", validates = {
-            @Validate(validator = Validators.ID, parameter = "id", failureCode = 1),
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "code", failureCode = 2),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "code", failureCode = 3),
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "key", failureCode = 4),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "key", failureCode = 5),
-            @Validate(validator = Validators.NOT_EMPTY, parameter = "name", failureCode = 6),
-            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "name", failureCode = 7),
-            @Validate(validator = Validators.SIGN),
-            @Validate(validator = ClassifyService.VALIDATOR_EXISTS, parameter = "id", failureCode = 9),
-            @Validate(validator = ClassifyService.VALIDATOR_CODE_KEY_NOT_EXISTS, parameters = {"code", "key", "id"}, failureCode = 10)
-    })
-    public Object modify() {
-        return templates.get().success(classifyService.modify(request.get("id"), request.get("code"), request.get("key"), request.get("value"), request.get("name"), request.getMap()), null);
-    }
-
     @Execute(name = "save", validates = {
             @Validate(validator = Validators.NOT_EMPTY, parameter = "code", failureCode = 2),
             @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "code", failureCode = 3),
@@ -97,7 +67,7 @@ public class ClassifyCtrl extends RecycleCtrlSupport {
             @Validate(validator = Validators.SIGN)
     })
     public Object save() {
-        return templates.get().success(classifyService.save(request.get("code"), request.get("key"), request.get("value"), request.get("name"), request.getMap()), null);
+        return templates.get().success(classifyService.save(request.setToModel(ClassifyModel.class)), null);
     }
 
     @Execute(name = "refresh", validates = {
