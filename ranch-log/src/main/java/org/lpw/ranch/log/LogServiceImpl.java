cef7f88f09030238fcfec87a04de35ba8e73402e
heisedebaise <heisedebaise@hotmail.com>
2019-01-22 18:23:46 +0800

    完善日志模块。

--- ranch-log/src/main/java/org/lpw/ranch/log/LogServiceImpl.java
+++ ranch-log/src/main/java/org/lpw/ranch/log/LogServiceImpl.java
@@ -1,23 +1,22 @@
 package org.lpw.ranch.log;
 
+import com.alibaba.fastjson.JSONObject;
 import org.lpw.ranch.user.helper.UserHelper;
 import org.lpw.tephra.ctrl.context.Header;
 import org.lpw.tephra.ctrl.context.Request;
 import org.lpw.tephra.scheduler.DateJob;
 import org.lpw.tephra.util.DateTime;
-import org.lpw.tephra.util.Json;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.Calendar;
+import java.util.Map;
 
 /**
  * @author lpw
  */
 @Service(LogModel.NAME + ".service")
 public class LogServiceImpl implements LogService, DateJob {
-    @Inject
-    private Json json;
     @Inject
     private DateTime dateTime;
     @Inject
@@ -35,10 +34,17 @@ public class LogServiceImpl implements LogService, DateJob {
         log.setType(type);
         log.setUser(userHelper.id());
         log.setIp(header.getIp());
-        log.setHeader(json.toObject(header.getMap(), false).toJSONString());
-        log.setParameter(json.toObject(request.getMap(), false).toJSONString());
+        log.setHeader(toJson(header.getMap()));
+        log.setParameter(toJson(request.getMap()));
         log.setTime(dateTime.now());
         logDao.save(log);
+    }
+
+    private String toJson(Map<String, String> map) {
+        JSONObject object = new JSONObject();
+        map.forEach(object::put);
+
+        return object.toJSONString();
     }
 
     @Override
