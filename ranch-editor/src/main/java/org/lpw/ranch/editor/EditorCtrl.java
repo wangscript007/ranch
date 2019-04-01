1024e2a18b658fc171efe1639c96b219cb2b3d83
heisedebaise <heisedebaise@hotmail.com>
2019-04-01 17:37:55 +0800

    完善编辑器模块。

--- ranch-editor/src/main/java/org/lpw/ranch/editor/EditorCtrl.java
+++ ranch-editor/src/main/java/org/lpw/ranch/editor/EditorCtrl.java
@@ -166,7 +166,7 @@ public class EditorCtrl {
             @Validate(validator = EditorService.VALIDATOR_EXISTS, parameter = "id", failureCode = 2),
             @Validate(validator = RoleService.VALIDATOR_VIEWABLE, parameter = "id", failureCode = 41),
             @Validate(validator = RoleService.VALIDATOR_PASSWORD, parameters = {"user", "id", "password"}, failureCode = 49),
-            @Validate(validator = EditorService.VALIDATOR_EXPORT, parameter = "id", failureCode = 15)
+//            @Validate(validator = EditorService.VALIDATOR_EXPORT, parameter = "id", failureCode = 15)
     })
     public Object pdf() {
         return editorService.pdf(request.get("id"), request.get("email"));
