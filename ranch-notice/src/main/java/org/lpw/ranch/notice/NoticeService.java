eb18d07332fce960d3f7b2faa0bd0514ea868523
heisedebaise <heisedebaise@hotmail.com>
2019-05-17 14:13:25 +0800

    完善公告模块。

--- ranch-notice/src/main/java/org/lpw/ranch/notice/NoticeService.java
+++ ranch-notice/src/main/java/org/lpw/ranch/notice/NoticeService.java
@@ -6,7 +6,7 @@ import com.alibaba.fastjson.JSONObject;
  * @author lpw
  */
 public interface NoticeService {
-    String ALL_USER = "global00-0000-0000-0000-000000000000";
+    String ALL_USER = "00000000-user-user-user-000000000000";
 
     /**
      * 检索全局通知集。
