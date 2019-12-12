483ed2315d9960190527c47f5d5d14f434fb36a1
heisedebaise <heisedebaise@hotmail.com>
2019-12-12 17:23:29 +0800

    忽略paypal。

--- ranch-paypal/src/main/java/org/lpw/ranch/paypal/transaction/TransactionServiceImpl.java
+++ ranch-paypal/src/main/java/org/lpw/ranch/paypal/transaction/TransactionServiceImpl.java
@@ -1,12 +1,6 @@
 package org.lpw.ranch.paypal.transaction;
 
 import com.alibaba.fastjson.JSONObject;
-import com.braintreepayments.http.serializer.Json;
-import com.paypal.core.PayPalEnvironment;
-import com.paypal.core.PayPalHttpClient;
-import com.paypal.orders.Money;
-import com.paypal.orders.Order;
-import com.paypal.orders.OrdersGetRequest;
 import org.lpw.ranch.payment.helper.PaymentHelper;
 import org.lpw.ranch.paypal.PaypalModel;
 import org.lpw.ranch.paypal.PaypalService;
@@ -53,44 +47,45 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public String verify(String key, String tradeNo) {
-        PaypalModel paypal = paypalService.findByKey(key);
-        try {
-            Order order = new PayPalHttpClient(new PayPalEnvironment.Live(paypal.getAppId(), paypal.getSecret()))
-                    .execute(new OrdersGetRequest(tradeNo)).result();
-            if (logger.isInfoEnable())
-                logger.info("校验PayPal[{}]支付[{}]结果[{}]。", key, tradeNo, order.status());
-            if (notSuccess(order.status()))
-                return null;
-
-            String result = new Json().serialize(order);
-            if (logger.isInfoEnable())
-                logger.info("校验PayPal[{}]支付[{}]结果[{}]。", key, tradeNo, result);
-            order.purchaseUnits().forEach(purchaseUnit -> {
-                if (purchaseUnit == null)
-                    return;
-
-                if (purchaseUnit.payments() == null || validator.isEmpty(purchaseUnit.payments().captures())) {
-//                    create(paypal, order.id(), purchaseUnit.amount().currencyCode(), purchaseUnit.amount().value(), result,
-//                            order.createTime(), order.updateTime());
-
-                    return;
-                }
-
-                purchaseUnit.payments().captures().forEach(capture -> {
-                    if (capture == null || notSuccess(capture.status()) || transactionDao.count(capture.id()) > 0)
-                        return;
-
-                    create(paypal, capture.id(), capture.amount().currencyCode(), capture.amount().value(), result,
-                            capture.createTime(), capture.updateTime());
-                });
-            });
-
-            return result;
-        } catch (Throwable throwable) {
-            logger.warn(throwable, "获取PayPal交易[{}:{}]信息发生异常！", key, tradeNo);
-
-            return null;
-        }
+//        PaypalModel paypal = paypalService.findByKey(key);
+//        try {
+//            Order order = new PayPalHttpClient(new PayPalEnvironment.Live(paypal.getAppId(), paypal.getSecret()))
+//                    .execute(new OrdersGetRequest(tradeNo)).result();
+//            if (logger.isInfoEnable())
+//                logger.info("校验PayPal[{}]支付[{}]结果[{}]。", key, tradeNo, order.status());
+//            if (notSuccess(order.status()))
+//                return null;
+//
+//            String result = new Json().serialize(order);
+//            if (logger.isInfoEnable())
+//                logger.info("校验PayPal[{}]支付[{}]结果[{}]。", key, tradeNo, result);
+//            order.purchaseUnits().forEach(purchaseUnit -> {
+//                if (purchaseUnit == null)
+//                    return;
+//
+//                if (purchaseUnit.payments() == null || validator.isEmpty(purchaseUnit.payments().captures())) {
+////                    create(paypal, order.id(), purchaseUnit.amount().currencyCode(), purchaseUnit.amount().value(), result,
+////                            order.createTime(), order.updateTime());
+//
+//                    return;
+//                }
+//
+//                purchaseUnit.payments().captures().forEach(capture -> {
+//                    if (capture == null || notSuccess(capture.status()) || transactionDao.count(capture.id()) > 0)
+//                        return;
+//
+//                    create(paypal, capture.id(), capture.amount().currencyCode(), capture.amount().value(), result,
+//                            capture.createTime(), capture.updateTime());
+//                });
+//            });
+//
+//            return result;
+//        } catch (Throwable throwable) {
+//            logger.warn(throwable, "获取PayPal交易[{}:{}]信息发生异常！", key, tradeNo);
+//
+//            return null;
+//        }
+        return null;
     }
 
     private boolean notSuccess(String status) {
